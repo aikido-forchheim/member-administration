7f444b3033548e69364652c6a7b18a769df601f3
Markus Malz <asgart79@gmail.com>
2017-01-29 12:48:20 +0100

    inital commit

---
+++ MemberAdministration/MemberAdministration/MemberAdministrationPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MemberAdministration
+{
+	public partial class MemberAdministrationPage : ContentPage
+	{
+		public MemberAdministrationPage()
+		{
+			InitializeComponent();
+		}
+	}
+}

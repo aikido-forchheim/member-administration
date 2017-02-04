290fd06640a5025a0d2db949fec32adbb016c744
Markus Malz <asgart79@gmail.com>
2017-02-04 16:59:24 +0100

    Continue on enter

--- MemberAdministration/MemberAdministration/Views/StartPage.xaml.cs
+++ MemberAdministration/MemberAdministration/Views/StartPage.xaml.cs
@@ -7,6 +7,14 @@ namespace MemberAdministration
 {
 	public partial class StartPage : ContentPage
 	{
+		void Handle_Completed(object sender, System.EventArgs e)
+		{
+			if ((this.BindingContext as StartPageViewModel).UserAdministrationCommand.CanExecute(null))
+			{
+				(this.BindingContext as StartPageViewModel).UserAdministrationCommand.Execute(null);
+			}
+		}
+
 		public StartPage()
 		{
 			InitializeComponent();
